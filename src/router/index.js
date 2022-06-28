9b4c8a4b1a65292581ad9f244f8cfc87406e2877
Teng-Xinde <2716004230@qq.com>
2022-06-28 08:55:28 +0800

    登录页完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,50 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login')
+  }
+  ,
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    // name: 'layout', // 如果父路由有默认子路由，那它的 name 没有意义
+    component: () => import('@/views/layout'),
+    children: [
+      {
+        path: '', // 默认子路由，只能有1个
+        name: 'home',
+        component: () => import('@/views/home')
+      },
+      {
+        path: '/home', // 默认子路由，只能有1个
+        name: 'home',
+        component: () => import('@/views/home')
+      },
+      {
+        path: 'qa',
+        name: 'qa',
+        component: () => import('@/views/qa')
+      },
+      {
+        path: 'video',
+        name: 'video',
+        component: () => import('@/views/video')
+      },
+      {
+        path: 'my',
+        name: 'my',
+        component: () => import('@/views/my')
+      }
+    ]
   }
+
+
 ]
 
 const router = new VueRouter({
